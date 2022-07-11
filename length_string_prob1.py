34d56a4192eb2d42bb440d7be4aa6536546de408
JafaryNm <jaffarymdegela@gmail.com>
2022-07-11 16:39:10 +0300

    Problem 1 Solution Updated

---
+++ length_string_prob1.py
@@ -0,0 +1,34 @@
+################### PROBLEM 1 ##################
+"""
+  Programm to find length of string
+  "" -- OUTPUT 0
+  "H" -- OUTPUT 1
+  "HELLO" -- OUTPUT 5
+  "Amazing" -- OUTPUT 7
+  
+  
+"""
+
+## Solve problem we need to use Built in Function len
+
+"""
+    len(s)
+    Return the length (the number of items) 
+    of an object. The argument may be a sequence
+    (such as a string, bytes, tuple, list, or range) or a 
+    collection (such as a dictionary, set, or frozen set).
+"""
+
+s="python"
+### Test with empty string ###
+
+empty_string=""
+h="h"
+hello="HELLO"
+amazing="Amazing"
+
+print(len(s))
+print(empty_string)
+print(h)
+print(hello)
+print(amazing)
