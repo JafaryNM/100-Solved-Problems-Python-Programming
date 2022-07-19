8b00bf7178da0fb9316f2ccc8ecb82d539f5c900
JafaryNm <jaffarymdegela@gmail.com>
2022-07-19 11:08:07 +0300

    Checking String contain digits

---
+++ string_contain_numbers.py
@@ -0,0 +1,41 @@
+##################### PROBLEM 6 ##########
+
+"""
+
+Write a Python program that check if a string only contains numbers.
+
+If it does, print True. Else, print False
+
+"""
+"""
+
+OUTPUT 
+ "Hello" --> Print out  "olleh"
+ "Woo"  --> Print out "oow"
+ ""   --> ""
+"""
+
+############### Solutiion ###################
+
+s="69"
+
+# Return is Decimal numbers
+
+# Return true If number is decimal or return false
+
+print(s.isdecimal())
+
+# Return true if string contains numbers
+
+print(s.isdigit())
+
+
+
+
+
+
+
+
+
+
+
